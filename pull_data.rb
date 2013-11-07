2b1642e5108538f8e177aa9c7bf04d87cfd3d804
cjlwired <carlos.jose.lazo@gmail.com>
2013-11-07 13:51:57 -0500

    Convert initial work to OO layout.

--- pull_data.rb
+++ pull_data.rb
@@ -2,19 +2,28 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
 
-week_url = "http://games.espn.go.com/ffl/leaders?leagueId=978650&teamId=1&scoringPeriodId=1&seasonId=2013"
+class FflScraper
+	
+	attr_accessor :doc
 
-doc = Nokogiri::HTML(open(week_url))
+	def initialize(url)
+		@doc = Nokogiri::HTML(open(url))
+	end
 
-total_records = doc.css('div.games-fullcol').css('table').css('tr').count        
+end
+
+# total_records = doc.css('div.games-fullcol').css('table').css('tr').count        
 
 #for row in 2..total_records-1
-    for col in 0..20
-
-        p pulled_row_data = doc.css('div.games-fullcol').css('table').css('tr')[2].css('td')[col].text
-        
-    end
-
-
+	# for col in 0..20
+	#   ap pulled_row_data = doc.css('div.games-fullcol').css('table').css('tr')[2].css('td')[col].text  
+	# end
 #end
+
+#########################
+# Perform Data Analysis #
+#########################
+
+ffl = FflScraper.new("http://games.espn.go.com/ffl/leaders?leagueId=978650&teamId=1&scoringPeriodId=1&seasonId=2013")
