de68599298551cf7734e2ad770d0ae1378f046a8
cjlwired <carlos.jose.lazo@gmail.com>
2013-11-07 14:14:30 -0500

    Create function to pull column information from each row.

--- pull_data.rb
+++ pull_data.rb
@@ -1,24 +1,34 @@
 #!/usr/bin/env ruby
 
+require 'awesome_print'
 require 'nokogiri'
 require 'open-uri'
-require 'awesome_print'
 
 class FflScraper
+
+	NUM_COL = 26
+	REJECT  = [1,3,4,7,12,16,21,25]
 	
-	attr_accessor :doc
+	attr_reader :doc, :tot_rec
 
 	def initialize(url)
-		@doc = Nokogiri::HTML(open(url))
+		@doc 		 = Nokogiri::HTML(open(url))
+		@tot_rec = doc.css('div.games-fullcol').css('table').css('tr').count
+	end
+
+	def collect_col_data(row)
+		data_cols = (1..NUM_COL).reject{|i| REJECT.include?(i)}.compact
+
+		data_cols.collect do |col|
+			doc.css('div.games-fullcol').css('table').css('tr')[row].css('td')[col].text  
+		end
 	end
 
 end
 
-# total_records = doc.css('div.games-fullcol').css('table').css('tr').count        
-
 #for row in 2..total_records-1
 	# for col in 0..20
-	#   ap pulled_row_data = doc.css('div.games-fullcol').css('table').css('tr')[2].css('td')[col].text  
+	#   ap pulled_row_data = doc.css('div.games-fullcol').css('table').css('tr')[row].css('td')[col].text  
 	# end
 #end
 
@@ -27,3 +37,4 @@ end
 #########################
 
 ffl = FflScraper.new("http://games.espn.go.com/ffl/leaders?leagueId=978650&teamId=1&scoringPeriodId=1&seasonId=2013")
+# ap ffl.collect_col_data(2)
